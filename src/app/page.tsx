4de6936f83feede952ca80e2be5922f09595fa83
SanyamPunia <lewarends@gmail.com>
2024-05-10 15:37:29 +0530

    break queries into separate files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
+import { getMyImages } from "~/server/queries";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/4a717935-7c6e-46ef-aa7a-7cd93614832a-umor0a.png",
-  "https://utfs.io/f/1228b2c9-17a4-43d5-b8e9-5854cea4ab0d-5poc22.png",
-  "https://utfs.io/f/a20460e6-d17c-457a-a074-cf8581eae107-kfa44l.png",
-  "https://utfs.io/f/430ef115-317e-47f7-91c1-b8a4031ac6fd-rddkd0.png",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 async function Images() {
-  const images = await db.query.images.findMany({
-    orderBy: (model, { desc }) => desc(model.id),
-  });
+  const images = await getMyImages();
 
   return (
     <div className="flex flex-wrap gap-4">
@@ -36,7 +23,9 @@ export default async function HomePage() {
   return (
     <main className="">
       <SignedOut>
-        <div className="text-center h-full w-full text-2xl">Please Sign In above</div>
+        <div className="h-full w-full text-center text-2xl">
+          Please Sign In above
+        </div>
       </SignedOut>
       <SignedIn>
         <Images />
