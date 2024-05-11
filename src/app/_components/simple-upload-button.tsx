63d847989e995892bdd5a29c22b0029a0dcea1e8
SanyamPunia <lewarends@gmail.com>
2024-05-11 15:29:15 +0530

    ratelimit implemented

--- src/app/_components/simple-upload-button.tsx
+++ src/app/_components/simple-upload-button.tsx
@@ -90,6 +90,11 @@ export function SimpleUploadButton() {
         },
       );
     },
+    onUploadError(error) {
+      posthog.capture("upload_error", { error });
+      toast.dismiss("upload-begin");
+      toast.error("Upload Failed");
+    },
     onClientUploadComplete(res) {
       toast.dismiss("upload-begin");
       toast(<span className="text-lg">Upload complete!</span>);
